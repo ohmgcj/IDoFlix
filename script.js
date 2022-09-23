da410bfdaf62f67a8ce3883f6b2914a00638f911
j4c2 <j4c.2@hotmail.com>
2022-09-22 21:26:46 -0300

    Código refatorado e adicionado funcionalidade de excluir filme

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 var count = 0
 
-function adicionarFilme() {
-  var campoFilm = document.getElementById("filmeImg").value;
-  var campoName = document.getElementById("filmeNome").value;
-  if (campoFilm.endsWith(".jpg") || campoFilm.endsWith(".jpeg")) {
-    listarFilmes(campoFilm, campoName, count);
+function listarFilmes(foto, nome, num) {
+  var list = document.getElementById("listaFilmes");
+  var pushFilmTag = `<img src= ${foto}>`;
+  var pushFilmName = `<div class="tituloFilme">${nome}</div>`;
+  var pushFilmBtn = `<button class="btnExcluir" onclick="removerFilme(${num})">Excluir</button>`
+  list.innerHTML += `<div id="${num}">${pushFilmTag} ${pushFilmName} ${pushFilmBtn}</div>`;
+}
+
+function validaExtensao(imagem, nome) {
+  if (imagem.endsWith(".jpg") || imagem.endsWith(".jpeg")) {
+    listarFilmes(imagem, nome, count);
     count++;
   } else {
     alert("erro");
   }
-
-  function listarFilmes(foto, nome, num) {
-    var list = document.getElementById("listaFilmes");
-    var pushFilmTag = `<img src= ${foto}>`;
-    var pushFilmName = `<div class="tituloFilme">${nome}</div>`;
-    list.innerHTML += `<div class="tela${num}">${pushFilmTag} ${pushFilmName}</div>`;
-  }
 }
 
-/*function removerFilme() {
-  const card = document.getElementById("")
-}*/
+function refId(id) {
+  return document.getElementById(id)
+}
+
+function limpaCampos(id) {
+  return refId(id).value = ""
+}
+
+function adicionarFilme() {
+  var campoFilm = refId("filmeImg").value
+  var campoName = refId("filmeNome").value
+
+  validaExtensao(campoFilm, campoName)
+  limpaCampos("filmeImg")
+  limpaCampos("filmeNome")
+}
+
+function removerFilme(id) {
+  const filme = refId(id)
+  return filme.remove()
+}
